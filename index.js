5cdf9f903b7be88f42afba6c37fefc8e560cdfd0
Thokozan Ngubane <mxolisithokozani7@gmail.com>
2022-06-14 15:56:06 +0200

    fixed gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburgermenu = document.querySelector('.nav-toggle');
+const menuNav = document.querySelector('.nav-menu');
+
+hamburgermenu.addEventListener('click', () => {
+  hamburgermenu.classList.toggle('active');
+  menuNav.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburgermenu.classList.remove('active');
+  })
+);
