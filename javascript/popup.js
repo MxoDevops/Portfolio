bc94a78d84d49e6f857765f8ef9622ceb94de197
Hamayoun Ali <humayoonali2@gmail.com>
2022-06-20 17:05:48 +0430

    Update popup.js
    I have added some js code in popup.js file.

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,1 +1,14 @@
+const hamburgermenu = document.querySelector('.nav-toggle');
+const menuNav = document.querySelector('.navbar ul');
 
+hamburgermenu.addEventListener('click', () => {
+  hamburgermenu.classList.toggle('active');
+  menuNav.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburgermenu.classList.remove('active');
+    menuNav.classList.remove('active');
+  })
+);
