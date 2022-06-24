a55233d217e2bce53754e07c5c033059a89448fa
Thokozan Ngubane <mxolisithokozani7@gmail.com>
2022-06-24 12:28:45 +0200

    updated readme file

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,3 +1,5 @@
+//form validation
+
 const formInput = document.getElementById('footer-form');
 const email = document.querySelector('#email');
 const errorElement = document.getElementById('error');
